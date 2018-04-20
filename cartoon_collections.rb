94548ea80e176e05b4cfc4117c2f3658646bba21
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-20 15:43:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? { |call| call.length >= 4 } : false 
+  calls.any? { |call| call.length >= 4 } true : false 
 end
 end 
 
