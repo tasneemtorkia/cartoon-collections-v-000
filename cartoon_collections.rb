46da3e83f254b8cc82b52939474547c413b2d6f7
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-20 15:43:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 def long_planeteer_calls(calls)
   calls.any? { |call| call.length >= 4 } : false 
 end
-
+end 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
