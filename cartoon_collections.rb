cc3e0b97cb04fbbef54f1307aa151ba696f33a44
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-20 15:34:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 end 
 
 def summon_captain_planet(array)
-  array.map { |planeteer| planeteer.capitalize + 
+  array.map { |planeteer| planeteer.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
