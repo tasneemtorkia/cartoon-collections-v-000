12df01673d4e0d734c88cfcbcd900025bcbcd2b4
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-20 19:16:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.find do |str| str.include?(cheese_types) 
+  ingredients.find do |str| cheese_types.include?(str) 
 end
 end 
